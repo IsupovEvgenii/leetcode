6586e8e80a29520614694fd2254a36d86a49232b
Isupov Evgenii <damk209@yahoo.com>
2018-03-28 21:54:49 +0300

    accepted

---
+++ 344ReverseString.go
@@ -0,0 +1,19 @@
+/*Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".*/
+
+
+func reverseString(s string) string {
+	result := ""
+	if s != "" {
+		for pos := len(s) - 1; pos > len(s) / 2; pos-- {
+			result += string(s[pos])
+		}
+		for pos := len(s) / 2; pos >= 0; pos-- {
+			result += string(s[pos])
+		}
+	}
+    
+	return result
+}
