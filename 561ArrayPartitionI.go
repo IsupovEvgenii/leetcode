54e63a9da44066239a493879f7d5192c77000da6
Isupov Evgenii <damk209@yahoo.com>
2018-03-29 22:53:22 +0300

    new tasks

---
+++ 561ArrayPartitionI.go
@@ -0,0 +1,11 @@
+import "sort"
+func arrayPairSum(nums []int) int {
+    sort.Slice(nums, func(i, j int) bool {
+        return nums[i] < nums[j]
+    })
+	result := 0
+	for i:= 0; i < len(nums); i += 2{
+		result += nums[i]
+	}
+	return result
+}
